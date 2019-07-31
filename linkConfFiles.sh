c13a5d1e26143827f8166314ad8b4871870f92ec
en0k <en0k@x30.arktinen>
2019-07-31 14:43:38 -0300

    fix path linkConfFiles

--- linkConfFiles.sh
+++ linkConfFiles.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-CLONEDREPO=$HOME/eins/i3
+CLONEDREPO=$HOME/myGitRemote/eins/i3
 I3DIR=$HOME/.config/i3
 SCREENDIR=$HOME/screenshots
 
 if [ -d "I3DIR" ] ; then
-	ln -sf $CLONEDREPO/config ~/.config/i3/
+   ln -sf "$CLONEDREPO"/config ~/.config/i3/
 else
-	mkdir -p $I3DIR
-	ln -sf $CLONEDREPO/config ~/.config/i3/
+   mkdir -p "$I3DIR"
+   ln -sf "$CLONEDREPO"/config ~/.config/i3/
 fi
 
 
 if [ ! -d "$SCREENDIR" ]; then
-	mkdir -p $HOME/screenshots/cut
+   mkdir -p "$HOME"/screenshots/cut
 fi
